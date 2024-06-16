9f73c35a2f7d0b3521488d007268f550777abb3a
Lexie <6937574+Lexiebean@users.noreply.github.com>
2024-06-16 17:45:51 +1000

    Removed debug code

--- Fix for spore.com links.user.js
+++ Fix for spore.com links.user.js
@@ -13,11 +13,8 @@ var a = document.querySelectorAll("a");
 for(var i = 0; i < a.length; i++){
 
 	regex = new RegExp('http.?:\/\/www.spore.com\/.+');
-
 	if (regex.test(a[i].href) == true) {
-
 		regex = /http.?:\/\/www.spore.com\//i;
-		//console.log(a[i].href.replace(regex, '/'));
 		a[i].href = a[i].href.replace(regex, '/')
 	}
 }
